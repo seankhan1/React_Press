e2fd6797a2eb94b5119ea77099385762e2477266
seankhan1 <seankhan74@gmail.com>
2017-12-25 12:00:00 +0500

    changes

---
+++ backend/middleware/fetchU.js
@@ -0,0 +1,22 @@
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = 'AyushAyushAyushok';
+
+
+const fetchuser = (req, res, next) => {
+    //get the user from the jwt token and addid to the request
+    const token = req.header('auth-token');
+    if (!token) {
+        res.status(401).send({ error: "Please authenticate using valid token" })
+    }
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+        req.user = data.user;
+        next();
+
+    } catch (error) {
+        res.status(401).send({ error: "Please authenticate using valid token" })
+    }
+
+}
+
+module.exports = fetchuser;
