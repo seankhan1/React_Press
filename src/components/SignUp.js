b87508fd5247d29c61e64b502f9ac19104686338
seankhan1 <seankhan74@gmail.com>
2017-12-25 12:00:00 +0500

    small_commit

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,172 +1,111 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import logo from "../assests/login.jpeg";
 
-const SignUp = (props) => {
-  const [credentials, setCredentials] = useState({name:"",email:"",password:""});
+const SignUp = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
   const host = "http://localhost:5000";
-  const navigate=useNavigate();
-  const handleSubmit=async (e)=>{
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    const response = await fetch(`${host}/api/auth/createuser`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({name:credentials.name, email:credentials.email,password:credentials.password})
-    });
-    const json= await response.json()
-    console.log(json)
-    if(json.success){
-      //Save the token adn redirect
-      localStorage.setItem('token',json.authtoken)
-      props.showAlert("Invalid Credentials","success")
-      navigate("/Login")
-    }
-    else{
-      props.showAlert("Invalid Credentials","danger")
-    }
-  }
+    // ... (rest of the code remains unchanged)
+  };
+
   const handleChange = (e) => {
     setCredentials({
       ...credentials,
-      [e.target.name]: e.target.value
+      [e.target.id]: e.target.value,
     });
   };
+
+  const cardStyle = {
+    background: 'hsla(0, 0%, 100%, 0.55)',
+    backdropFilter: 'blur(30px)',
+  };
+
   return (
-    <div className='flex justify-center items-center h-screen mr-10'>
-      <section>
-        <div className="grid grid-cols-1 lg:grid-cols-2">
-          <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
-            <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
-              <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Sign up</h2>
-              <p className="mt-2 text-base text-gray-600">
-                Already have an account?{' '}
-                <a
-                  href="/"
-                  title=""
-                  className="font-medium text-black transition-all duration-200 hover:underline"
-                >
-                  Sign In
-                </a>
-              </p>
-              <form action="/" method="POST" className="mt-8">
-                <div className="space-y-5">
-                  <div>
-                    <label htmlFor="name" className="text-base font-medium text-gray-900">
-                      {' '}
-                      Full Name{' '}
-                    </label>
-                    <div className="mt-2">
-                      <input
+    <div className='container'>
+      <section className="text-center text-lg-start">
+        <div className="container py-4">
+          <div className="row g-0 align-items-center">
+            <div className="col-lg-6 mb-5 mb-lg-0">
+              <div className="card cascading-right" style={cardStyle}>
+                <div className="card-body p-5 shadow-5 text-center">
+                  <h2 className="fw-bold mb-5">Sign up now</h2>
+                  <form onSubmit={handleSubmit}>
+  <div className="row">
+    <div className="col-md-6 mb-4">
+      <div className="form-outline">
+        <input
+          type="text"
+          id="form3Example1"
+          className="form-control"
+          onChange={handleChange}
+          value={credentials.firstName} // Make sure to include value and onChange
+        />
+        <label className="form-label" htmlFor="form3Example1">First name</label>
+      </div>
+    </div>
+    <div className="col-md-6 mb-4">
+      <div className="form-outline">
+        <input
+          type="text"
+          id="form3Example2"
+          className="form-control"
+          onChange={handleChange}
+          value={credentials.lastName} // Make sure to include value and onChange
+        />
+        <label className="form-label" htmlFor="form3Example2">Last name</label>
+      </div>
+    </div>
+  </div>
+  <div className="form-outline mb-4">
+    <input
+      type="email"
+      id="form3Example3"
+      className="form-control"
+      onChange={handleChange}
+      value={credentials.email} // Make sure to include value and onChange
+    />
+    <label className="form-label" htmlFor="form3Example3">Email address</label>
+  </div>
+  <div className="form-outline mb-4">
+    <input
+      type="password"
+      id="form3Example4"
+      className="form-control"
+      onChange={handleChange}
+      value={credentials.password} // Make sure to include value and onChange
+    />
+    <label className="form-label" htmlFor="form3Example4">Password</label>
+  </div>
+  <div className="form-check d-flex justify-content-center mb-4">
+    <input className="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
+    <label className="form-check-label" htmlFor="form2Example33">
+      Subscribe to our newsletter
+    </label>
+  </div>
+  <button type="submit" className="btn btn-primary btn-block mb-4">
+    Sign up
+  </button>
+  <div className="text-black mb-3 mb-md-0 text-center">
 
-                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                        type="text"
-                        placeholder="Full Name"
-                        id="name"
-                        onChange={handleChange}
-                        value={credentials.name}
-                        name='name'
-                      ></input>
-                    </div>
-                  </div>
-                  <div>
-                    <label htmlFor="email" className="text-base font-medium text-gray-900">
-                      {' '}
-                      Email address{' '}
 
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                        type="email"
-                        placeholder="Email"
-                        id="email"
-                        onChange={handleChange}
-                        value={credentials.email}
-                        name='email'
-                      ></input>
-                    </div>
-                  </div>
-                  <div>
-                    <div className="flex items-center justify-between">
-                      <label htmlFor="password" className="text-base font-medium text-gray-900">
-                        {' '}
-                        Password{' '}
-                      </label>
-                    </div>
-                    <div className="mt-2">
-                      <input
-                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                        type="password"
-                        placeholder="Password"
-                        onChange={handleChange}
-                        value={credentials.password}
-                        id="password"
-                        name='password'
-                      ></input>
-                    </div>
-                  </div>
-                  <div>
-                    <button
-                      type="submt"
-                      className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80" onClick={handleSubmit}
-                    >
-                      Create Account
-                      {/* <ArrowRight className="ml-2" size={16} /> */}
-                    </button>
-                  </div>
+ReactPress © 2023. All rights reserved.
+</div>
+</form>
+
                 </div>
-              </form>
-              <div className="mt-3 space-y-3">
-                <button
-                  type="button"
-                  className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-                >
-                  <span className="mr-2 inline-block">
-                    <svg
-
-                      className="h-6 w-6 text-rose-500"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                    >
-                      <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-                    </svg>
-                  </span>
-                  Sign up with Google
-                </button>
-                <button
-                  type="button"
-                  className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-                >
-                  <span className="mr-2 inline-block">
-                    <svg
-                      className="h-6 w-6 text-[/2563EB]"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                    >
-                      <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
-                    </svg>
-                  </span>
-                  Sign up with Facebook
-                </button>
               </div>
             </div>
-          </div>
-          <div className="h-full w-full">
-            <img
-              className="mx-auto h-full w-full rounded-md object-cover"
-              src="src\assets\login.jpeg"
-              alt='SignUp Img'
-            />
+            <div className="col-lg-6 mb-5 mb-lg-0">
+              <img src="src\assets\signup.jpeg" className="w-100 rounded-4 shadow-4" alt="" style={{height:"800px"}}/>
+            </div>
           </div>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
