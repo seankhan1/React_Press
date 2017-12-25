486c7d38385b1088307637a3c37106cf182da87f
seankhan1 <seankhan74@gmail.com>
2017-12-25 12:00:00 +0500

    getuserdetails

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -42,3 +42,16 @@ router.post('/createuser', [
         res.status(500).json({ error: error.message });
     }
 });
+router.post('/getuser',fetchuser, async (req, res) => {
+ 
+    try{
+        const userId=req.user.id; 
+        const user=await User.findById(userId).select("-password")
+        res.send(user)
+    } catch(error){
+        res.status(500).send("internal server error");
+    }
+})
+
+
+module.exports = router;
